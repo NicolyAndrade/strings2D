6d222205f72d756974c36e363664a037637f1912
NicolyAndrade <neves.andrade@escola.pr.gov.br>
2025-08-26 14:58:54 +0000

    aula 6

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
+    
+    campoResultado.textContent = palavrasChave.join(", ");
+    
+}
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contafrequencias(palavras) ;
+     let ordenadas  = Object.keys(frequencias).sort();
+}
+  // código omitido{};
 
-  const palavras = texto.split(" ");
-  campoResultado.textContent = palavras;
+    
+     for (let j of palavras) 
+    
+      if (i == j) {
+        frequencias[i]++;
+      }
+    
   
+  for (let i of palavras){
+  frequencias [i] = 0
+ }
+  
+    console.log(frequencias);
+    return palavras;
+  }
+
+  function contafrequencias(palavras){
+
+  }let frequencias = {};
+    
+     for (let j of palavras) 
+    
+      if (i == j) {
+        frequencias[i]++;
+      }
+    
+  
+  for (let i of palavras){
+  frequencias [i] = 0
+ 
+  
+    //console.log(frequencias);
+    return frequencias;
 }
