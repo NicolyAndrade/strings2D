2cdc3b44b4cbbe23c3adc392eb52135d532c2d57
NicolyAndrade <neves.andrade@escola.pr.gov.br>
2025-09-02 15:00:06 +0000

    aula 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins.js"
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
@@ -12,6 +13,10 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    for (let i in palavras){
+      palavras [i]=palavras[i].toLowerCase();//LETRAS MINUSCULAS 
+    }
+    palavras = tiraPalavrasRuins (palavras);
     const frequencias = contafrequencias(palavras) ;
      let ordenadas  = Object.keys(frequencias).sort();
 }
@@ -50,4 +55,18 @@ function processaTexto(texto){
   
     //console.log(frequencias);
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+
+  const palavrasBoas = [];
+
+  for (let palavra of palavras) {
+    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+
+  return palavrasBoas;
 }
